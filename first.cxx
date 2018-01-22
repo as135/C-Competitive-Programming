34037381f16db19578fd092ca6dc71f33a0c4fa1
as135 <as135@rice.edu>
2018-01-22 00:05:05 -0600

    Added first

---
+++ first.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input;
+	cin >> input;
+	int firstNum = 3;
+	int secondNum = 32;
+	int product = firstNum * secondNum * input;
+	cout << "Hi you've just used an amazing programm! \n";
+	cout << product + 10032; 
+	return 0;
+} 
+
