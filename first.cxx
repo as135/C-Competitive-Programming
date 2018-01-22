568921bf0f7b7838070e5db8871db7d9eb025bff
Arjoon Srikanth <as135@rice.edu>
2018-01-22 01:08:03 -0600

    changed number

--- first.cxx
+++ first.cxx
@@ -5,7 +5,7 @@ int main() {
 	int input;
 	cin >> input;
 	int firstNum = 3;
-	int secondNum = 32;
+	int secondNum = 332;
 	int product = firstNum * secondNum * input;
 	cout << "Hi you've just used an amazing programm! \n";
 	cout << product + 10032; 
