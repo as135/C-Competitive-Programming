6cd6b9bdce8da5ef0bf355a665244b9bc3181090
Arjoon Srikanth <as135@rice.edu>
2018-01-22 15:43:56 -0600

    update first

--- first.cxx
+++ first.cxx
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * This function is a comment, and should be ignored by the compiler
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+int fuck(int a, int b) {
+	return a * b;
+	
+}
+
 int main() {
 	int input;
+	cout << "Please input a number, and it will be magically transformed \n";
 	cin >> input;
 	int firstNum = 3;
 	int secondNum = 332;
 	int product = firstNum * secondNum * input;
 	cout << "Hi you've just used an amazing programm! \n";
-	cout << product + 10032; 
+	cout << product + 10032 << '\n'; 
+	cout << fuck(firstNum, input);
 	return 0;
 } 
 
